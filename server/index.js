852d59eec96b15cfd2352f9b74a497bc6aa1acc7
Neeraj <nk5696974@gmail.com>
2024-04-09 10:16:34 +0530

    new project

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+
+app.use("/", (req, res) => {
+    res.send("Server is running")
+})
+
+
+app.listen(5000, console.log("Server is running on 5000"))
